70a3c7f74cdc435d57777557442bd9a9120daab0
metakey <altmetakey@gmail.com>
2016-07-13 22:54:29 +0800

    前端完成，没做到public页面的上传本地文件

--- src/main/java/com/netease/marketOnline/controller/UploadPictureController.java
+++ src/main/java/com/netease/marketOnline/controller/UploadPictureController.java
@@ -1,13 +1,56 @@
 package com.netease.marketOnline.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/*
+ * 文档有问题？ * 没有成功
+ *  var xhr = new XMLHttpRequest();
+    xhr.open("post", "/api/upload", true);
+	ar o = JSON.parse(xhr.responseText);
+	imageUrl = o && o.result;
+	image.value = imageUrl;
+	imgpre.src = imageUrl;
+ */
 @Controller
+@RequestMapping("/api")
 public class UploadPictureController {
-	@RequestMapping("upload")
-	public void uploadPicture(@RequestParam("id") String url) {
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+	@ResponseBody
+	public Object uploadPicture(@RequestParam("id") String url, HttpSession session) {
+		Map<String, Object> result=new HashMap<String, Object>();
+		String userName=(String)session.getAttribute("username");
+		Integer usertype=(Integer)session.getAttribute("usertype");
+		if (!judgeSeller(userName,usertype)) {
+			result.put("code", 400);
+			result.put("message", "bad seller");
+			result.put("result", false);
+		} else {//假定上传成功
+			System.out.println(url);
+			result.put("code", 200);
+			result.put("result", true);
+//		} else { //上传失败时
+//			result.put("code", 400);
+//			result.put("message", "上传失败");
+//			result.put("result", false);
+		}
 		
+		return result;
+	}
+	
+	private boolean judgeSeller(String userName,int usertype){
+		boolean isSeller = false;
+		if (usertype==1 && userName.equals("seller")) {
+			isSeller = true;
+		}
+		return isSeller;
 	}
 }
--- src/main/java/com/netease/marketOnline/web/controller/Public.java
+++ src/main/java/com/netease/marketOnline/web/controller/Public.java
@@ -1,5 +1,28 @@
 package com.netease.marketOnline.web.controller;
 
-public class Public {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.netease.marketOnline.meta.User;
+
+@Controller
+public class Public {
+	@RequestMapping(value="/public", method=RequestMethod.GET)
+	public String publicPage(HttpSession session, Model model){
+		String userName=(String)session.getAttribute("username");
+		Integer usertype=(Integer)session.getAttribute("usertype");
+		User user = null;
+		if(userName!=null && usertype==1){
+			user=new User();
+			user.setUsername(userName);
+			user.setUsertype(usertype);
+		}
+		model.addAttribute("user", user);
+		return "public";
+	}
+	
 }
